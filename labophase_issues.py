242bc310afc7ba27551882acf0af1d014c0adaa7
Vinícius de Lima Gonçalves <viniciusslimaa@outlook.com>
2025-03-27 17:49:39 -0300

    TODO to Issue

---
+++ labophase_issues.py
@@ -0,0 +1,12 @@
+# TODO: ctrl+alt+del == show options to shutdown, suspend, restart, lock, logout etc...
+
+# TODO: lid close not suspend but not change display to hdmi
+
+# TODO: or generate a .lilith_ip file syncthing between host or automatic change ip in config file (systemd)
+
+# TODO: install and configurate:
+# - Zotero
+# - Obsidian
+# - Syncthing
+
+# TODO: tryna copy edge setting that not being sync to make default profile
